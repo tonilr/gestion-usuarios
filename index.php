7e18433ba4eb04374970ae8e570c52efacd1e747
Toni <l.r.toni@gmail.com>
2021-04-06 19:36:21 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de usuarios</title>
+</head>
+<body>
+    <form action="validar.php">
+        <label for="username">Username
+            <input type="text" name="username" id="username">
+        </label>
+        <label for="password">Password
+            <input type="text" name="password" id="password">
+        </label>
+        <input type="submit" class="boton">
+    </form>
+</body>
+</html>
