7fa2b8fe35c4c1f88053796fbd70a02ed0c725f7
Toni <l.r.toni@gmail.com>
2021-04-06 22:10:59 +0200

    Form to add user and sanitize and control system added

---
+++ db/action-signup.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+//Check if all the fields are set
+if (!isset($_POST["username"]) or $_POST["username"]==NULL or !isset($_POST["email"]) or $_POST["email"]==NULL or !isset($_POST["name"]) or $_POST["name"]==NULL or !isset($_POST["password"]) or $_POST["password"]==NULL){
+    $_SESSION["fieldMissing"]=1;
+    header ("Location: ../signup.php");
+}else{
+    $username=filter_var($_POST["username"],FILTER_SANITIZE_STRING);
+    $email=filter_var($_POST["email"],FILTER_SANITIZE_EMAIL);
+    $email=filter_var($email,FILTER_SANITIZE_STRING);
+    $name=filter_var($_POST["name"],FILTER_SANITIZE_STRING);
+    $userpassword=hash('sha256',$_POST["password"]);
+    //Check if one of the fields is not valid
+    if($username!=$_POST["username"] or $email!=$_POST["email"] or $name!=$_POST["name"]){
+        $_SESSION["fieldsError"]=1;
+        header ("Location: ../signup.php");
+    }else{
+        //Conection to the database
+        $server="localhost";
+        $user="project_nascor";
+        $password="zXd)JX]rMHCgNyu0";
+        $bbdd="project_nascor";
+        $table="users";
+        //Query
+        $sql="INSERT INTO $table VALUES(NULL, '$username', '$email', '$name', '$userpassword', 1, current_timestamp())";
+        //Connection
+        $conn = new mysqli($server, $user, $password, $bbdd);
+        //Check the connection
+        if ($conn->connect_error){
+            return "Connection error: $conn->connect_error";
+        }
+        //Query to insert new user
+        if ($conn->query($sql)){
+            $conn->close();
+            $_SESSION["userAdded"]=1;
+            header ("Location: ../");
+        }else{
+            $_SESSION["errorAdding"]=1;
+            header ("Location: ../signup.php");
+        }
+    }
+
+}
+?>
--- signup.php
+++ signup.php
@@ -5,37 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Users Managment</title>
+    <title>Sign Up</title>
 </head>
 <body>
     <section class="mainContent">
-        <h1>Sing in with your account</h1>
+        <h1>Create an account</h1>
         <?php
         session_start();
         if (isset($_SESSION["fieldMissing"]) and $_SESSION["fieldMissing"]==1){
             echo "<p class='alert'>You must fill all the fields in the form</p>";
-            $_SESSION["fieldsMissing"]=0;
+            $_SESSION["fieldMissing"]=0;
         }
         if (isset($_SESSION["fieldsError"]) and $_SESSION["fieldsError"]==1){
             echo "<p class='alert'>You have entered an invalid format in one field</p>";
             $_SESSION["fieldsError"]=0;
         }
         if (isset($_SESSION["userAdded"]) and $_SESSION["userAdded"]==1){
-            echo "<p class='alert'>User added correctly!</p>";
+            echo "<p class='alert'>Account created correctly!</p>";
             $_SESSION["userAdded"]=0;
         }
+        if (isset($_SESSION["errorAdding"]) and $_SESSION["errorAdding"]==1){
+            echo "<p class='alert'>There was an error adding the user account!</p>";
+            $_SESSION["errorAdding"]=0;
+        }
         ?>
-        <form action="db/action-signin.php">
-            <label for="username">Username
+        <form action="db/action-signup.php" method="post" class="signupForm">
+            <label for="username">User Name
                 <input type="text" name="username" id="username">
             </label>
+            <label for="email">Email
+                <input type="text" name="email" id="email">
+            </label>
+            <label for="name">Name
+                <input type="text" name="name" id="name">
+            </label>
             <label for="password">Password
-                <input type="text" name="password" id="password">
+                <input type="password" name="password" id="password">
             </label>
             <input type="submit" class="boton">
         </form>
-    <p>Don't have an account? Sign up <a href="signup.php">here</a></p>
     </section>
-
 </body>
 </html>
